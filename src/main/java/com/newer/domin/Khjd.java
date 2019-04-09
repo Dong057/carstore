ad1715bbfc41e57c613669af6e957e70d89c50f2
ls94 <339823543@qq.com>
2019-04-09 14:41:16 +0800

    客户订单实体类

---
+++ src/main/java/com/newer/domin/Khjd.java
@@ -0,0 +1,208 @@
+package com.newer.domin;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class Khjd {
+    private Integer ddbh;
+    private Integer clid;
+    private Integer khbh;
+    private Date dgrq;
+    private String jdzt;
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date yjsj;
+    private String cjh;
+    private String fdjh;
+    private String fplx;
+    private Double fpje;
+    private Double cljg;
+    private Double gzs;
+    private Double bxf;
+    private Double zsf;
+    private Double spf;
+    private Integer shr;
+    private Date shrq;
+    private Double ysk;
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date kprq;
+    private String hth;
+    private String sfzf;
+    private String bz;
+
+    public Integer getDdbh() {
+        return ddbh;
+    }
+
+    public void setDdbh(Integer ddbh) {
+        this.ddbh = ddbh;
+    }
+
+    public Integer getClid() {
+        return clid;
+    }
+
+    public void setClid(Integer clid) {
+        this.clid = clid;
+    }
+
+    public Integer getKhbh() {
+        return khbh;
+    }
+
+    public void setKhbh(Integer khbh) {
+        this.khbh = khbh;
+    }
+
+    public Date getDgrq() {
+        return dgrq;
+    }
+
+    public void setDgrq(Date dgrq) {
+        this.dgrq = dgrq;
+    }
+
+    public String getJdzt() {
+        return jdzt;
+    }
+
+    public void setJdzt(String jdzt) {
+        this.jdzt = jdzt;
+    }
+
+    public Date getYjsj() {
+        return yjsj;
+    }
+
+    public void setYjsj(Date yjsj) {
+        this.yjsj = yjsj;
+    }
+
+    public String getCjh() {
+        return cjh;
+    }
+
+    public void setCjh(String cjh) {
+        this.cjh = cjh;
+    }
+
+    public String getFdjh() {
+        return fdjh;
+    }
+
+    public void setFdjh(String fdjh) {
+        this.fdjh = fdjh;
+    }
+
+    public String getFplx() {
+        return fplx;
+    }
+
+    public void setFplx(String fplx) {
+        this.fplx = fplx;
+    }
+
+    public Double getFpje() {
+        return fpje;
+    }
+
+    public void setFpje(Double fpje) {
+        this.fpje = fpje;
+    }
+
+    public Double getCljg() {
+        return cljg;
+    }
+
+    public void setCljg(Double cljg) {
+        this.cljg = cljg;
+    }
+
+    public Double getGzs() {
+        return gzs;
+    }
+
+    public void setGzs(Double gzs) {
+        this.gzs = gzs;
+    }
+
+    public Double getBxf() {
+        return bxf;
+    }
+
+    public void setBxf(Double bxf) {
+        this.bxf = bxf;
+    }
+
+    public Double getZsf() {
+        return zsf;
+    }
+
+    public void setZsf(Double zsf) {
+        this.zsf = zsf;
+    }
+
+    public Double getSpf() {
+        return spf;
+    }
+
+    public void setSpf(Double spf) {
+        this.spf = spf;
+    }
+
+    public Integer getShr() {
+        return shr;
+    }
+
+    public void setShr(Integer shr) {
+        this.shr = shr;
+    }
+
+    public Date getShrq() {
+        return shrq;
+    }
+
+    public void setShrq(Date shrq) {
+        this.shrq = shrq;
+    }
+
+    public Double getYsk() {
+        return ysk;
+    }
+
+    public void setYsk(Double ysk) {
+        this.ysk = ysk;
+    }
+
+    public Date getKprq() {
+        return kprq;
+    }
+
+    public void setKprq(Date kprq) {
+        this.kprq = kprq;
+    }
+
+    public String getHth() {
+        return hth;
+    }
+
+    public void setHth(String hth) {
+        this.hth = hth;
+    }
+
+    public String getSfzf() {
+        return sfzf;
+    }
+
+    public void setSfzf(String sfzf) {
+        this.sfzf = sfzf;
+    }
+
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz;
+    }
+}
